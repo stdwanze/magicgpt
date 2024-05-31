9dfabcdadb9e1fa7c64261f97d858bf002433a0d
Stefan Eggers <stefan.eggers@ibsolution.de>
2024-05-31 10:55:40 +0200

    vol

--- play.js
+++ play.js
@@ -1,6 +1,6 @@
 var player = require('play-sound')(opts = {})
 
 // $ mplayer foo.mp3
-player.play('output.wav', function(err){
+player.play('output.wav', { play: [ '-v', 30 ] }, function(err){
   if (err) throw err
 })
