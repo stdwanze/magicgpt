ef8320bef018ef23eedf06bd37ad15e3d1d46576
Stefan Eggers <stefan.eggers@ibsolution.de>
2024-06-01 14:44:03 +0200

    added tts

---
+++ speak.js
@@ -0,0 +1,17 @@
+var espeak = require('espeak');
+ 
+const fs = require("fs");
+// optionally set the path to the `espeak` cli program if it's not in your PATH
+//espeak.cmd = '/usr/bin/espeak';
+ 
+espeak.speak('hello world', function(err, wav) {
+  if (err) return console.error(err);
+  
+  // get the raw binary wav data
+  var buffer = wav.buffer;
+  
+  // get a base64-encoded data URI
+  //var dataUri = wav.toDataUri();
+
+  fs.writeFileSync("speaknode.wav",buffer);
+});
