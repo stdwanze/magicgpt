057566187289b4ab6b069501f25d0e10db05a452
Stefan Eggers <stefan.eggers@ibsolution.de>
2024-06-01 14:23:41 +0200

    rhino pv

--- runpv.js
+++ runpv.js
@@ -6,41 +6,53 @@ const fs = require("node:fs");
 const {Rhino } = require("@picovoice/rhino-node");
 const accessKey = access;
 
-const recog = new Rhino(accessKey , resolve("MagicGPT_de_raspberry-pi_v3_0_0.rhn"),0.5,5.0, true,resolve("rhino_params_de.pv") );
-const fileStream = fs.createWriteStream("output.wav", { encoding: 'binary' });
 
-const frameLength = recog.frameLength;
+async function micDemo() {
+
+  const recog = new Rhino(accessKey , resolve("MagicGPT_de_raspberry-pi_v3_0_0.rhn"),0.5,5.0, true,resolve("rhino_params_de.pv") );
+  const fileStream = fs.createWriteStream("output.wav", { encoding: 'binary' });
+
+  const frameLength = recog.frameLength;
 
 
-const devices = PvRecorder.getAvailableDevices();
-for (let i = 0; i < devices.length; i++) {
-    console.log(`index: ${i}, device name: ${devices[i]}`);
-}
-const recorder = new PvRecorder(frameLength, 0);
-recorder.start();
-
-console.log("Context info:");
-console.log("-------------");
-console.log(handle.getContextInfo());
-
-console.log(
-  `Listening for speech within the context of '${contextName}'. Please speak your phrase into the microphone. `
-);
-console.log("Press ctrl+c to exit.")
-
-while (!isInterrupted) {
-  const pcm = await recorder.read();
-  const isFinalized = recog.process(pcm);
-  if (isFinalized === true) {
-    let inference = recog.getInference();
-    console.log("Inference result:");
-    console.log(JSON.stringify(inference, null, 4));
-    console.log();
+  const devices = PvRecorder.getAvailableDevices();
+  for (let i = 0; i < devices.length; i++) {
+      console.log(`index: ${i}, device name: ${devices[i]}`);
   }
-}
+  const recorder = new PvRecorder(frameLength, 0);
+  recorder.start();
 
-console.log("Stopping...");
-recorder.stop();
-recorder.release();
+  console.log("Context info:");
+  console.log("-------------");
+  console.log(handle.getContextInfo());
+
+  console.log(
+    `Listening for speech within the context of '${contextName}'. Please speak your phrase into the microphone. `
+  );
+  console.log("Press ctrl+c to exit.")
+
+  while (!isInterrupted) {
+    const pcm = await recorder.read();
+    const isFinalized = recog.process(pcm);
+    if (isFinalized === true) {
+      let inference = recog.getInference();
+      console.log("Inference result:");
+      console.log(JSON.stringify(inference, null, 4));
+      console.log();
+    }
+  }
+
+  console.log("Stopping...");
+  recorder.stop();
+  recorder.release();
+
+}
+(async function () {
+  try {
+      await micDemo();
+  } catch (e) {
+      console.error(e.toString());
+  }
+})();
 
 
