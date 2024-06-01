64b78235ad7511d37243a237046e232c607c2bab
Stefan Eggers <stefan.eggers@ibsolution.de>
2024-06-01 15:11:07 +0200

    fixes

--- speak.js
+++ speak.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 //espeak.cmd = '/usr/bin/espeak';
  
 function genSpeak(text){
-  espeak.speak('richtig', ['-vde+f3'], function(err, wav) {
+  espeak.speak(text, ['-vde+f3'], function(err, wav) {
     if (err) return console.error(err);
     
     // get the raw binary wav data
