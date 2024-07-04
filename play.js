08c69d6539c0cc15902c3f5978d6873307f8047d
Stefan Eggers <stefan.eggers@ibsolution.de>
2024-07-04 15:39:04 +0200

    upped volume

--- play.js
+++ play.js
@@ -1,6 +1,6 @@
 var player = require('play-sound')(opts = { player: "play"})
 
 // $ mplayer foo.mp3
-player.play('output.wav', { play: [ '-v', 30 ] }, function(err){
+player.play('output.wav', { play: [ '-v', 50 ] }, function(err){
   if (err) throw err
 })
